56b10e1765ed35d5912e20e066428df958d66b96
mnguyen0811 <minhnguyen08112004@gmail.com>
2025-05-21 08:07:02 +0700

    create new

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass {
+    public static void main(String[] args) {
+        // Nội dung của chương trình
+        int a = 3;
+        int b = 2;
+        System.out.println("Java!");
+        System.out.println("s : %d",a + b);
+    }
+}
