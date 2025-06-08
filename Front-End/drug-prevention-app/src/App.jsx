96896e1a5ab3e223761f2e4166bde6725285fa97
ITduow <duonghtse180223@fpt.edu.vn>
2025-06-08 16:19:04 +0700

    updateupdate

--- Front-End/drug-prevention-app/src/App.jsx
+++ Front-End/drug-prevention-app/src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
-import "./App.css";
-import Header from "./components/PageHeader/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MemberBookedConsultations from "./pages/MemberBookedConsultations/MemberBookedConsultations";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/PageHeader/Header'; 
+import HomePage from './pages/HomePage/HomePage'; 
+import MemberBookedConsultations from './pages/MemberBookedConsultations/MemberBookedConsultations';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="app-container">
         <Header />
         <div className="content-container">
           <Routes>
-            {/* <Route path="/" element={<h1>Home Page</h1>} /> */}
-            <Route
-              path="/booked-consultations"
-              element={<MemberBookedConsultations />}
-            />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/booked-consultations" element={<MemberBookedConsultations />} />
+            <Route path="*" element={<div>404 - Không tìm thấy trang</div>} />
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
